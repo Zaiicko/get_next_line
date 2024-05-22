216d4a92f1f067420c2ee0f28c545dbaafa8f297
Mehdi Skrabel <meskrabe@fu-r1-p5.s19.be>
2024-05-22 18:53:18 +0200

    Add buffer .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:33:14 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/05/22 16:36:58 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/05/22 18:31:48 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 #include "stdlib.h"
 
