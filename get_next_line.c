2fb72ab9481bab6fa7223370d8c94d93f9c4d8cd
Mehdi Skrabel <meskrabe@fu-r1-p5.s19.be>
2024-05-24 19:57:01 +0200

    Add substr

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,28 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:33:12 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/05/24 01:15:18 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:56:08 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	str = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_memcpy(str, s + start, len);
+	return (str);
+}
 
 char	*get_next_line(int fd)
 {
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 16:33:14 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/05/24 18:44:46 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/05/24 19:56:37 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ size_t	ft_strlen(char *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
